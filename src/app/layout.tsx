1fcbecc2cfa85a2013e02c20053ed77659208a86
mouhamedbaba <lamottelymouhamed@gmail.com>
2024-07-07 11:55:54 +0000

    fix : set default theme to system

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -61,7 +61,7 @@ export default function RootLayout({
           fontSans.variable
         )}
       >
-        <ThemeProvider attribute="class" defaultTheme="light">
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
           <TooltipProvider delayDuration={0}>
             {children}
             <Navbar />
