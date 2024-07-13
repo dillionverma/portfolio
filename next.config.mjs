eb6c1e3c32e2769239c27895739bcc93ced7d98f
Robbie Radev <robbie.radev@gmail.com>
2024-07-13 15:29:24 +0300

    portfolio changes

--- next.config.mjs
+++ next.config.mjs
@@ -4,3 +4,11 @@ const nextConfig = {
 };
 
 export default nextConfig;
+
+module.exports = {
+  output: 'export',
+  // Base path for GitHub Pages
+  basePath: '/robbieradev.github.io/',
+  // Asset prefix for GitHub Pages
+  assetPrefix: '/robbieradev.github.io/',
+}
