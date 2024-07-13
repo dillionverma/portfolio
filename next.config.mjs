a20e7d73c9e99591f2785607e58413d9dbf28cfe
Robbie Radev <robbie.radev@gmail.com>
2024-07-13 15:43:16 +0300

    ne znam, brat

--- next.config.mjs
+++ next.config.mjs
@@ -4,11 +4,3 @@ const nextConfig = {
 };
 
 export default nextConfig;
-
-module.exports = {
-  output: 'export',
-  // Base path for GitHub Pages
-  basePath: '/robbieradev.github.io/',
-  // Asset prefix for GitHub Pages
-  assetPrefix: '/robbieradev.github.io/',
-}
