4de942154eebba4077888120d1cb98a7d372d9ad
Ali Bektash <agokselb@gmail.com>
2024-08-15 20:29:41 +0900

    fix: :fire: Replaced isInView value
    IntersectionObserver error has been fixed

--- src/components/magicui/blur-fade.tsx
+++ src/components/magicui/blur-fade.tsx
@@ -25,14 +25,10 @@ const BlurFade = ({
   delay = 0,
   yOffset = 6,
   inView = false,
-  inViewMargin = "-50px",
   blur = "6px",
 }: BlurFadeProps) => {
   const ref = useRef(null);
-  const inViewResult = useInView(ref, {
-    once: true,
-    margin: `${Number(inViewMargin)}px`,
-  });
+  const inViewResult = true;
   const isInView = !inView || inViewResult;
   const defaultVariants: Variants = {
     hidden: { y: yOffset, opacity: 0, filter: `blur(${blur})` },
