83227f9ebdf44874c0a1397e0c1a2fdb5d734ba2
Robin Augereau <102476446+newtondotcom@users.noreply.github.com>
2025-03-27 17:25:23 +0100

    fix: change default contact mean

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -190,10 +190,10 @@ export default function Page() {
               <p className="mx-auto max-w-[600px] text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
                 Want to chat? Just shoot me a dm{" "}
                 <Link
-                  href={DATA.contact.social.X.url}
+                  href={DATA.contact.social.LinkedIn.url}
                   className="text-blue-500 hover:underline"
                 >
-                  with a direct question on twitter
+                  with a direct question on LinkedIn
                 </Link>{" "}
                 and I&apos;ll respond whenever I can. I will ignore all
                 soliciting.
